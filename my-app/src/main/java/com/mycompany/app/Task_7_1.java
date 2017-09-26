ea14a1bb4a19732f11ad6939ed8249d5e85e68b7
adanilov <alex.daniels.au@gmail.com>
2017-09-26 19:19:40 +0700

    7.1 committed

---
+++ my-app/src/main/java/com/mycompany/app/Task_7_1.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+public class Task_7_1 {
+
+    public static String intToString(int x) {
+        // put your solution here
+    }
+
+    public static int stringToInt(String s) {
+        // put your solution here
+    }
+
+}
---
+++ my-app/src/test/java/com/mycompany/app/Task_7_1_Test.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import junit.framework.TestCase;
+
+public class Task_7_1_Test extends TestCase {
+
+
+    public void test0() {
+        assertTrue(Task_7_1.intToString(345).equals("345"));
+    }
+
+    public void test1() {
+        assertTrue(Task_7_1.stringToInt("345") == 345);
+    }
+
+}
