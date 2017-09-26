ad9d137e2b394593b348db12993cd58b619debe6
adanilov <alex.daniels.au@gmail.com>
2017-09-26 19:31:32 +0700

    a better test

--- my-app/src/test/java/com/mycompany/app/Task_7_1_Test.java
+++ my-app/src/test/java/com/mycompany/app/Task_7_1_Test.java
@@ -6,11 +6,11 @@ public class Task_7_1_Test extends TestCase {
 
 
     public void test0() {
-        assertTrue(Task_7_1.intToString(345).equals("345"));
+        assertTrue(Task_7_1.intToString(-345).equals("-345"));
     }
 
     public void test1() {
-        assertTrue(Task_7_1.stringToInt("345") == 345);
+        assertTrue(Task_7_1.stringToInt("-345") == -345);
     }
 
 }
