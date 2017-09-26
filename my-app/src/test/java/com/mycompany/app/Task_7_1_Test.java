72b8b4c90cec3240fb3087388bc6aa0cb2ad6f7d
adanilov <alex.daniels.au@gmail.com>
2017-09-26 19:50:05 +0700

    better tests

--- my-app/src/test/java/com/mycompany/app/Task_7_1_Test.java
+++ my-app/src/test/java/com/mycompany/app/Task_7_1_Test.java
@@ -6,10 +6,18 @@ public class Task_7_1_Test extends TestCase {
 
 
     public void test0() {
-        assertTrue(Task_7_1.intToString(-345).equals("-345"));
+        assertTrue(Task_7_1.intToString(345).equals("345"));
     }
 
     public void test1() {
+        assertTrue(Task_7_1.intToString(-345).equals("-345"));
+    }
+
+    public void test2() {
+        assertTrue(Task_7_1.stringToInt("345") == 345);
+    }
+
+    public void test3() {
         assertTrue(Task_7_1.stringToInt("-345") == -345);
     }
 
