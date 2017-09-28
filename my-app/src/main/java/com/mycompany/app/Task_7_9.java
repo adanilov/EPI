c8c8607e297e467a113281fcd81b1cf0d2326283
adanilov <alex.daniels.au@gmail.com>
2017-09-29 00:38:13 +1000

    7.9

---
+++ my-app/src/main/java/com/mycompany/app/Task_7_9.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task_7_9 {
+
+    public static int romanToInteger(String s) {
+
+        Map<Character, Integer> T = new HashMap<Character, Integer>() {
+            {
+                put('I', 1);
+                put('V', 5);
+                put('X', 10);
+                put('L', 50);
+                put('C', 100);
+                put('D', 500);
+                put('M', 1000);
+            }
+        };
+
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ my-app/src/test/java/com/mycompany/app/Task_7_9_Test.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import junit.framework.TestCase;
+
+public class Task_7_9_Test extends TestCase {
+
+    public void test0() {
+        int integer = Task_7_9.romanToInteger("IC");
+        assertTrue(integer == 99);
+    }
+}
