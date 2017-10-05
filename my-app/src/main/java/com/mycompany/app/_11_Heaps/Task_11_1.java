ed90d4f7911a9f69710d67caf925198ce9a3e5e5
adanilov <alex.daniels.au@gmail.com>
2017-10-05 22:05:37 +1100

    11.1 amended

--- my-app/src/main/java/com/mycompany/app/_11_Heaps/Task_11_1.java
+++ my-app/src/main/java/com/mycompany/app/_11_Heaps/Task_11_1.java
@@ -4,16 +4,6 @@ import java.util.List;
 
 public class Task_11_1 {
 
-    private static class ArrayEntry {
-        public Integer value;
-        public Integer arrayId;
-
-        public ArrayEntry(Integer value, Integer arrayId) {
-            this.value = value;
-            this.arrayId = arrayId;
-        }
-    }
-
     public static List<Integer> mergeSortedArrays(List<List<Integer>> sortedArrays) {
         throw new UnsupportedOperationException();
     }
